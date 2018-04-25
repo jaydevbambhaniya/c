4554b8476f28f6c2c5712746b0d0c7110067284e
Mohit Sharma <38177203+Mohit-Sharma1@users.noreply.github.com>
2018-04-25 18:26:37 +0530

    Add files via upload

---
+++ ASCII char with their values.cpp
@@ -0,0 +1,13 @@
+//Wap a C program to print all ASCII character with their values//
+#include<stdio.h>
+main()
+{
+	int i;
+	printf("ASCII Chars with their values\n");
+	for(i=0;i<=255;i++)
+	{
+		printf("%d=%c\n",i);
+	}
+}
+
+
